4abf65321b154bb7d155b8317ca7e2d7dcc4b0e6
Artos Web <teodor.at.tod@gmail.com>
2022-07-09 00:42:02 +0300

    multiple changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import './App.css'
 import Footer from './components/common/Footer';
 import Header from "./components/common/Header";
 import Search from './components/Search/Search';
-import UserSection from './components/UserSection/UserSection';
+import UserList from './components/UserList/UserList';
 
+import { useEffect, useState } from 'react';
 
+const baseUrl = 'http://localhost:3005/api';
 
 function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(`${baseUrl}/users`)
+            .then(res => res.json())
+            .then(result => {
+                setUsers(result.users)
+            })
+    }, [])
+    console.log(users);
+
     return (
         <div>
             <Header />
 
             <main className="main">
                 <section className="card users-container">
-                <Search />
-                <UserSection />
+                    <Search />
+                    <UserList users={users}/>
                 </section>
 
             </main>
