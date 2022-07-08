de7e1c2698ce541d092b379ccec97c553ed77345
Artos Web <teodor.at.tod@gmail.com>
2022-07-08 21:25:18 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Footer from './components/common/Footer';
+import Header from "./components/common/Header";
+import Search from './components/Search/Search';
+import UserSection from './components/UserSection/UserSection';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                <Search />
+                <UserSection />
+                </section>
+
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
